a0e08d5cbb409d8c73e4d0dfe65601f15e725b6d
trix <trix@dr.dk>
2014-09-24 15:30:41 +0200

    video tag i forskellige versioner i VisOrdCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,20 @@
-angular.module('dagensord.controllers', ['ngAudio'])
+angular.module('dagensord.controllers', [])
 .controller('MainCtrl', function($scope) {
 
     $scope.title = '<a href="#/app/home"><img src="img/LOGO.png"></a>';
+/*
+    $scope.test = function() {
+        var element =
+            'Navigator '+navigator.userAgent+ '\n' +
+            'Device Name: '     + device.name     + '\n' +
+            'Device Cordova: '  + device.cordova + '\n' +
+            'Device Platform: ' + device.platform + '\n' +
+            'Device UUID: '     + device.uuid     + '\n' +
+            'Device Model: '    + device.model     + '\n' +
+            'Device Version: '  + device.version  + '\n';
+        alert("Properties:\n"+element);
+    }
+*/
 
 })
 
@@ -25,13 +38,44 @@ angular.module('dagensord.controllers', ['ngAudio'])
     $scope.ord = ord;
 })
 
-.controller('VisOrdCtrl', function($scope, $stateParams, dagensord) {
+.controller('VisOrdCtrl', function($scope, $stateParams, dagensord, $sce) {
     $scope.vistord = dagensord[0];
+        var imagename;
         if ($scope.vistord['image']) {
+            imagename = $scope.vistord['image'];
             $scope.vistord['image'] = "<img src='"+ imageurl + $scope.vistord['image']+"' alt='"+$scope.vistord['performer']+"'/>"
+        }
+        if ($scope.vistord['qbrick']) {
+            var videoSource = $scope.vistord.video.aoshq['0'];
+            var mimetype = $scope.vistord.video.aoshq['@attributes']['mimetype'];
+            var tester = 'PC';
+
+            if(navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)){
+
+                if(device.platform =="Android"){
+                    videoSource = $scope.vistord.video.android['0'];
+                    mimetype = $scope.vistord.video.android['@attributes']['mimetype'];
+                    tester = 'Android';
+                }
+                if(device.platform =="iPhone"){
+                    videoSource = $scope.vistord.video.iphone['0'];
+                    mimetype = $scope.vistord.video.iphone['@attributes']['mimetype'];
+                    tester = 'iPhone';
+                }
+                if(device.platform =="iPad"){
+                    videoSource = $scope.vistord.video.ipad['0'];
+                    mimetype = $scope.vistord.video.ipad['@attributes']['mimetype'];
+                    tester = 'iPad';
+                }
+            }
+            alert(tester);
+
+            var sourceString = "<source src='"+videoSource+"' type='"+mimetype+"'>";
+            $scope.vistord['videotag'] = $sce.trustAsHtml("<video controls poster='"+imageurl + imagename+"' width='100%' height='auto'>"+sourceString+"</video>");
         }
         console.log(dagensord[0]);
         console.log($scope.vistord['image']);
+        console.log($scope.vistord['videotag']);
 })
 
 .controller('SalmerCtrl', function($scope,salmer) {
